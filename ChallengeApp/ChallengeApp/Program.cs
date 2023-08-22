7b8f27410a0f7e8940c8c5860bd07977df390df7
Arkadiusz K <arkadini@live.com>
2023-08-22 21:56:35 +0200

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lekcja 4
+
+
+var Name = "Ewa";
+var Gender = "kobieta";
+var Age = 29;
+
+
+if (Age<30 && Gender=="kobieta" )
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (Age == 33)
+{
+    Console.WriteLine(Name + ", lat 33");
+}
+else if (Age <18 &&  Gender=="mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Pozostałe");
+}
