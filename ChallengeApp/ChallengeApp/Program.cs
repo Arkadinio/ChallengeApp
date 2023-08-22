bc28b1d9fadb46611505d9a6f5630aca2517cc17
Arkadiusz K <arkadini@live.com>
2023-08-22 21:58:04 +0200

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -6,7 +6,7 @@ var Gender = "kobieta";
 var Age = 29;
 
 
-if (Age<30 && Gender=="kobieta" )
+if (Age < 30 && Gender == "kobieta")
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
@@ -14,7 +14,7 @@ else if (Age == 33)
 {
     Console.WriteLine(Name + ", lat 33");
 }
-else if (Age <18 &&  Gender=="mężczyzna")
+else if (Age < 18 && Gender == "mężczyzna")
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
