e176dad454b6a7c159e59ca1c5dfb8e38ac087c1
Arkadiusz K <arkadini@live.com>
2023-08-26 08:17:31 +0200

    Dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,24 +1,73 @@
-﻿// Lekcja 4
+﻿// Zadanie 5
 
+int number = 67426;
+string NumberInString = number.ToString();
+char[] letters = NumberInString.ToCharArray();
 
-var name = "Ewa";
-var gender = "kobieta";
-var age = 29;
+int number0 = 0;
+int number1 = 0;
+int number2 = 0;
+int number3 = 0;
+int number4 = 0;
+int number5 = 0;
+int number6 = 0;
+int number7 = 0;
+int number8 = 0;
+int number9 = 0;
 
+foreach (char theNumber in letters)
+{
+    if (theNumber == '0')
+    {
+        number0++;
+    }
+    else if (theNumber == '1')
+    {
+        number1++;
+    }
+    else if (theNumber == '2')
+    {
+        number2++;
+    }
+    else if (theNumber == '3')
+    {
+        number3++;
+    }
+    else if (theNumber == '4')
+    {
+        number4++;
+    }
+    else if (theNumber == '5')
+    {
+        number5++;
+    }
+    else if (theNumber == '6')
+    {
+        number6++;
+    }
+    else if (theNumber == '7')
+    {
+        number7++;
+    }
+    else if (theNumber == '8')
+    {
+        number8++;
+    }
+    else if (theNumber == '9')
+    {
+        number9++;
+    }
 
-if (age < 30 && gender == "kobieta")
-{
-    Console.WriteLine(gender + " poniżej 30 lat");
+    
 }
-else if (age == 33 && name == "Ewa")
-{
-    Console.WriteLine(name + ", lat 33");
-}
-else if (age < 18 && gender == "mężczyzna")
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else
-{
-    Console.WriteLine("Pozostałe");
-}
+
+Console.WriteLine("0 => " + number0);
+Console.WriteLine("1 => " + number1);
+Console.WriteLine("2 => " + number2);
+Console.WriteLine("3 => " + number3);
+Console.WriteLine("4 => " + number4);
+Console.WriteLine("5 => " + number5);
+Console.WriteLine("6 => " + number6);
+Console.WriteLine("7 => " + number7);
+Console.WriteLine("8 => " + number8);
+Console.WriteLine("9 => " + number9);
