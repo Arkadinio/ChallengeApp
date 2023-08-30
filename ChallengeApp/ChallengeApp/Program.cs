0d27884ff4e8bebeb28deef9f4173b4236e2a575
Arkadiusz K <arkadini@live.com>
2023-08-30 08:25:04 +0200

    Dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,73 +1,42 @@
-﻿// Zadanie 5
+﻿using ChallengeApp;
 
-int number = 67426;
-string NumberInString = number.ToString();
-char[] letters = NumberInString.ToCharArray();
+Employee employee1 = new Employee("Jan", "As",30);
+Employee employee2 = new Employee("Monika", "Czyszka",22);
+Employee employee3 = new Employee("Zuzia", "Sandał", 15);
 
-int number0 = 0;
-int number1 = 0;
-int number2 = 0;
-int number3 = 0;
-int number4 = 0;
-int number5 = 0;
-int number6 = 0;
-int number7 = 0;
-int number8 = 0;
-int number9 = 0;
 
-foreach (char theNumber in letters)
+employee1.AddScore(5);
+employee1.AddScore(9);
+employee1.AddScore(1);
+
+employee2.AddScore(2);
+employee2.AddScore(1);
+employee2.AddScore(2);
+
+employee3.AddScore(2);
+employee3.AddScore(3);
+employee3.AddScore(3);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (theNumber == '0')
-    {
-        number0++;
-    }
-    else if (theNumber == '1')
-    {
-        number1++;
-    }
-    else if (theNumber == '2')
-    {
-        number2++;
-    }
-    else if (theNumber == '3')
-    {
-        number3++;
-    }
-    else if (theNumber == '4')
-    {
-        number4++;
-    }
-    else if (theNumber == '5')
-    {
-        number5++;
-    }
-    else if (theNumber == '6')
-    {
-        number6++;
-    }
-    else if (theNumber == '7')
-    {
-        number7++;
-    }
-    else if (theNumber == '8')
-    {
-        number8++;
-    }
-    else if (theNumber == '9')
-    {
-        number9++;
-    }
+    employee1, employee2, employee3
+};
 
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
+
+    }
 
 }
 
-Console.WriteLine("0 => " + number0);
-Console.WriteLine("1 => " + number1);
-Console.WriteLine("2 => " + number2);
-Console.WriteLine("3 => " + number3);
-Console.WriteLine("4 => " + number4);
-Console.WriteLine("5 => " + number5);
-Console.WriteLine("6 => " + number6);
-Console.WriteLine("7 => " + number7);
-Console.WriteLine("8 => " + number8);
-Console.WriteLine("9 => " + number9);
+Console.WriteLine("Największą ilość punktów posiada użytkownik " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surrname + " wiek " + employeeWithMaxResult.Age + " z ilością punktów " + employeeWithMaxResult.Result);
+
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,46 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+
+        public string Surrname { get; private set; }
+
+        public int Age { get; private set; }
+
+
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surrname, int age)
+        {
+            this.Name = name;
+            this.Surrname = surrname;
+            this.Age = age;
+
+        }
+
+
+
+
+
+
+        public void AddScore(int number)
+        {
+           
+            this.score.Add(number);
+
+        }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+
+            }
+        }
+
+
+
+    }
+}
