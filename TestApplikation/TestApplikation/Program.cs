143f85134088c91ee00a0797ab192883c8b510bd
Arkadinio <142746156+Arkadinio@users.noreply.github.com>
2023-09-02 11:27:11 +0200

    Delete TestApplikation directory

--- TestApplikation/TestApplikation/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using TestApplikation;
-
-Car car1 = new Car("Jan", "As", 30);
-Car car2 = new Car("2", "5555", 530);
-Car car3 = new Car("3", "241241", 430);
-
-Console.WriteLine(car1.Marka);
-
